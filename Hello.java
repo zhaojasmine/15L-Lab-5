1835c4216cac290465758fb66c2b837c7d4efba0
Jasmine <jaz008@ucsd.edu>
2015-02-11 19:12:05 -0800

    Using ARGV

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main( String argv[] )
     {
-        System.out.println( "Hello, World" );
+        System.out.println( "Hello, " + argv[0] + "!" );
     }
 }
