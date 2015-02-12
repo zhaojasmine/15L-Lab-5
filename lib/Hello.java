c3a34d73a19899569d6a620a9bf88da840ff3463
Jasmine <jaz008@ucsd.edu>
2015-02-11 19:39:14 -0800

    Hello uses Greeter

--- lib/Hello.java
+++ lib/Hello.java
@@ -4,13 +4,13 @@ public class Hello
     public static void main( String argv[] )
     {
 	// Default is "World"
-	// Author: Jasmine (jaz008@ucsd.edu)
         String name = "World";
         if ( argv.length != 0 )
         {
             name = argv[0];
         }
 
-        System.out.println( "Hello, " + name + "!" );
+	Greeter greeter = new Greeter(name);
+        System.out.println(greeter.greet() );
     }
 }
